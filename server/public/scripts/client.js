c58bf8e374eb95b0e7708666ae4d1843df009556
Michael Gregoire <mdgregoire@gmail.com>
2018-02-13 21:10:40 -0600

    got database in the loop

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,21 @@ console.log('js');
 
 $(document).ready(function(){
   console.log('jq');
+  writeJokes();
 })
+
+
+function writeJokes() {
+  console.log('inwritejokes');
+  $.ajax({
+    type: 'GET',
+    url: '/joke'
+  })
+  .done(function(response){
+    console.log('getwassuccesssful');
+    $('#outputDiv').text(response);
+  })
+  .fail(function(error){
+    console.log(error);
+  })
+}
--- server/routers/jokeRouter.js
+++ server/routers/jokeRouter.js
@@ -7,7 +7,19 @@ const bodyParser = require('body-parser');
 
 console.log('in router');
 
-
+router.get('/', function(request, response){
+  console.log('injoke get');
+  const sqlText = 'SELECT * FROM jokes';
+  pool.query(sqlText)
+  .then(function(result){
+    console.log('got result', result);
+    response.send(result);
+  })
+  .catch(function(error){
+    console.log('error on GET', error);
+    response.sendStatus(500);
+  })
+})//end get joke router
 
 
 
