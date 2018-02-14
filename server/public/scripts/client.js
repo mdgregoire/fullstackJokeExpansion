a9a8314be32efe117cf95ebdf8fdde589291666b
Michael Gregoire <mdgregoire@gmail.com>
2018-02-13 23:13:13 -0600

    promode done, not hard mode though

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,22 +1,81 @@
-console.log('js');
 
 $(document).ready(function(){
-  console.log('jq');
-  writeJokes();
+  onReady();
 })
 
+function onReady(){
+  $('#addJokeButton').on('click', function(){
+    getNewJoke();
+  });//end addJokeButton
+  getJokes();
+  $('#outputDiv').on('click', '.jokeOutput', function(){
+    let indexToDelete = $(this).index('.jokeOutput');
+    deleteJoke(indexToDelete);
+  });
+}//end onReady function
 
-function writeJokes() {
-  console.log('inwritejokes');
+function deleteJoke(index){
+  $.ajax({
+    type: 'POST',
+    url: '/joke/delete',
+    data: {data : index}
+  })
+  .done(function(response){
+    console.log('deletewassuccess', response);
+    getJokes();
+  })
+  .fail(function(error){
+    console.log(error);
+  });
+}//end deleteJoke
+
+function getJokes() {
   $.ajax({
     type: 'GET',
     url: '/joke'
   })
   .done(function(response){
-    console.log('getwassuccesssful');
-    $('#outputDiv').text(response);
+    console.log('getwassuccesssful', response);
+    jokeArray = response;
+    writeJokes(response);
   })
   .fail(function(error){
     console.log(error);
   })
-}
+}//end getJokes
+
+function getNewJoke(){
+  console.log('ingetnewjoke');
+  $.ajax({
+    type: 'POST',
+    url: '/joke/add',
+    data: {
+        whos_joke: $('#whoseJokeIn').val(),
+        joke_question: $('#questionIn').val(),
+        punch_line: $('#punchlineIn').val(),
+        date_added: $('#date_addedIn').val(),
+        funniness:  parseInt($('#funniness').val())
+          }
+  }).done(function(response){
+    console.log('getNewJokePost Success', response);
+    getJokes();
+  }).fail(function(response){
+    console.log('getNewJokePost fail', response);
+  });
+
+}//end getNewJoke post function
+
+function writeJokes(array){
+  $('#whoseJokeIn').val('');
+  $('#questionIn').val('');
+  $('#punchlineIn').val('');
+  $('#date_addedIn').val('');
+  $('#funniness').val('');
+  $('#outputDiv').empty();
+  console.log('inwritejokes');
+  for(i=0;i<array.length;i++){
+    let stringToAppend = '<li class = "jokeOutput">';
+    stringToAppend += array[i].whos_joke+': '+array[i].joke_question+' '+array[i].punch_line+' <b>Date Added:</b> '+array[i].date_added.substring(0,10)+' <b>Funniness:</b> '+array[i].funniness+'</li>';
+    $('#outputDiv').append(stringToAppend);
+  }
+}//end writeJokes
--- server/routers/jokeRouter.js
+++ server/routers/jokeRouter.js
@@ -2,18 +2,16 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 const bodyParser = require('body-parser');
-
-
-
-console.log('in router');
+let jokeArray=[];
 
 router.get('/', function(request, response){
   console.log('injoke get');
   const sqlText = 'SELECT * FROM jokes';
   pool.query(sqlText)
   .then(function(result){
-    console.log('got result', result);
-    response.send(result);
+    console.log('got result', result.rows);
+    jokeArray=result.rows;
+    response.send(result.rows);
   })
   .catch(function(error){
     console.log('error on GET', error);
@@ -21,7 +19,35 @@ router.get('/', function(request, response){
   })
 })//end get joke router
 
+router.post('/add', function(request, response){
+  const joke = request.body;
+  const sqlText = `INSERT INTO jokes
+      (whos_joke, joke_question, punch_line, date_added, funniness)
+      VALUES ($1, $2, $3, $4, $5)`;
+    pool.query(sqlText, [joke.whos_joke, joke.joke_question, joke.punch_line, joke.date_added, joke.funniness])
+    .then( (result) => {
+      console.log('added joke', result);
+      response.sendStatus(201);
+    })
+    .catch( (error) => {
+      console.log('error adding joke', error);
+    })
+})//end post joke router
 
+router.post('/delete', function(request, response){
+  const indexToDelete = request.body.data;
+  idToDelete = jokeArray[indexToDelete].id;
+  const sqlText = `DELETE FROM jokes WHERE id = ${idToDelete}`;
+  pool.query(sqlText)
+  .then(function(result){
+    console.log('deleted', idToDelete);
+    response.sendStatus(200);
+  })
+  .catch(function(error){
+    console.log('error on delete', error);
+    response.sendStatus(500);
+  })
+})//end delete joke router
 
 
 module.exports = router;
